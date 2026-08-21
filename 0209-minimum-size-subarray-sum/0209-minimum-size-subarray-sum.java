ef130beaa59a52eedd797df76d74a3bf5f9430a4
Sameer Kumar Singh <sameercoder01@gmail.com>
2026-08-21 23:26:40 +0530

    Time: 3 ms (3.67%), Space: 69.1 MB (67.39%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,33 +1,79 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        // In case of variable size window low=0 and high=0 lena hoga vahi min poss. size of window hoga i.e, 1
         int low = 0;
         int high = 0;
+        int result = Integer.MAX_VALUE;
 
         int sum = 0;
 
-        int res = Integer.MAX_VALUE;
-        boolean check = false;
-
         while(high < nums.length){
-            // Sabse pehle koi hiring hui nhi hai to hiring krwayenge
-            sum = sum + nums[high]; // hiring will happen only by using high pointer
-            
-            // Ab jab ekbaar hiring ho gii hai to dekhenge ki kya ye hired value >= target ko satisfy kr rha hai ya nhi
-            while(sum >= target){
-                check = true;
-                int len = high-low+1;
-                res = Math.min(len,res);
+            sum += nums[high];
 
-                sum = sum-nums[low];
+            while(sum >= target){
+                int len = high-low+1;
+                result = Math.min(result,len);
+                sum -= nums[low];
                 low++;
             }
-
-            high++; //
+            high++;
         }
-        if(check){
-            return res;
-        }else{
+        if(result == Integer.MAX_VALUE){
             return 0;
+        }else{
+            return result;
         }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     int low = 0;
+    //     int high = 0;
+
+    //     int sum = 0;
+
+    //     int res = Integer.MAX_VALUE;
+    //     boolean check = false;
+
+    //     while(high < nums.length){
+    //         // Sabse pehle koi hiring hui nhi hai to hiring krwayenge
+    //         sum = sum + nums[high]; // hiring will happen only by using high pointer
+            
+    //         // Ab jab ekbaar hiring ho gii hai to dekhenge ki kya ye hired value >= target ko satisfy kr rha hai ya nhi
+    //         while(sum >= target){
+    //             check = true;
+    //             int len = high-low+1;
+    //             res = Math.min(len,res);
+
+    //             sum = sum-nums[low];
+    //             low++;
+    //         }
+
+    //         high++; //
+    //     }
+    //     if(check){
+    //         return res;
+    //     }else{
+    //         return 0;
+    //     }
+    // }
