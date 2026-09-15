3132fd6e313581fc7ab287784c715da2062d606f
Sameer Kumar Singh <sameercoder01@gmail.com>
2026-09-15 12:23:49 +0530

    Time: 6 ms (47.56%), Space: 46.7 MB (90.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,22 +2,26 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
 
         int[] arr = new int[2];
+        
+
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    arr[0] = i;
-                    arr[1] = j;
-                    break;
-                }
+            map.put(nums[i],i);
+        }
+
+        for(int i=0; i<nums.length; i++){
+            int diff = target-nums[i];
+            if(map.containsKey(diff) && map.get(diff) != i){
+                arr[0] = i;
+                arr[1] = map.get(diff);
             }
         }
+
         return arr;
-        
         // Arrays.sort(nums);
         // int[] arr = new int[2];
-        // int i=0;
-        // int j = nums.length-1;
 
+        // int i=0; int j = nums.length-1;
         // while(i < j){
         //     int sum = nums[i] + nums[j];
         //     if(sum > target){
@@ -25,9 +29,24 @@ class Solution {
         //     }else if(sum < target){
         //         i++;
         //     }else{
-        //         arr[0] = i;
-        //         arr[1] = j;
+        //         arr[0] = nums[i];
+        //         arr[1] = nums[j];
         //         break;
+        //     }
+        // }
+
+        // arr[0] = map.get(arr[0]);
+        // arr[1] = map.get(arr[1]);
+        // return arr;
+
+        // int[] arr = new int[2];
+        // for(int i=0; i<nums.length; i++){
+        //     for(int j=i+1; j<nums.length; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             arr[0] = i;
+        //             arr[1] = j;
+        //             break;
+        //         }
         //     }
         // }
         // return arr;
